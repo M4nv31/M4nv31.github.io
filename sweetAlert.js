b5a27eb789f382077ce2bb9ea53a666b529c1a6a
M4nv31 <109698621+M4nv31@users.noreply.github.com>
2022-07-25 20:56:02 -0600

    Add files via upload

---
+++ sweetAlert.js
@@ -0,0 +1,9 @@
+Swal.fire({
+    title: 'Welcome to Our web site',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+  })
